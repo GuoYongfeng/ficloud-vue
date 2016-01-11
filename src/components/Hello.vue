2d9271ee99019a360893109d32e893f996b895d1
guoyff <guoyff@yonyou.com>
2016-01-11 21:04:38 +0800

    save

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -8,15 +8,7 @@
 export default {
   data () {
     return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-<<<<<<< HEAD
       msg: '一起玩转Vue.js'
-=======
-      msg: 'Hello World!'
->>>>>>> 350ddc37bf43e0c05a30ae5c645a48e2b0063ea9
     }
   }
 }
